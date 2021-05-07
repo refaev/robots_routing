eaa0a61371af6540d8c4ed7df5d2a987a4ba4aa7
Rafi Vivanti <vivanti@gmail.com>
2021-05-07 15:07:06 +0300

    with source and target in visualizer.py

--- manager.py
+++ manager.py
@@ -14,15 +14,15 @@ def main():
 
     map_2d, s0, t0, s1, t1, S = simulator.build_map('empty', W, H)
     map_3d = my_solver.solve_two_robots_routing(map_2d, s0, t0, s1, t1, l)
-    visualizer.show_3d_as_video(map_3d)
+    visualizer.show_3d_as_video(map_3d, s0, t0, s1, t1)
 
     map_2d, s0, t0, s1, t1, S = simulator.build_map('obstacle', W, H)
     map_3d = my_solver.solve_two_robots_routing(map_2d, s0, t0, s1, t1, l)
-    visualizer.show_3d_as_video(map_3d)
+    visualizer.show_3d_as_video(map_3d, s0, t0, s1, t1)
 
     map_2d, s0, t0, s1, t1, S = simulator.build_map('corridor', W, H)
     map_3d = my_solver.solve_two_robots_routing(map_2d, s0, t0, s1, t1, l)
-    visualizer.show_3d_as_video(map_3d)
+    visualizer.show_3d_as_video(map_3d, s0, t0, s1, t1)
 
 
 if __name__ == '__main__':
--- visualizer.py
+++ visualizer.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 
 
-def show_3d_as_video(map_3d):
+def show_3d_as_video(map_3d, s0, t0, s1, t1):
     for i in range(map_3d.shape[2]):
         im = map_3d[:, :, i]
         plt.clf()
         plt.imshow(im)
+        plt.plot(s0[1], s0[0], 'og')
+        plt.plot(t0[1], t0[0], 'xg')
+        plt.plot(s1[1], s1[0], 'oy')
+        plt.plot(t1[1], t1[0], 'xy')
         plt.title(str(i))
         plt.pause(0.01)
